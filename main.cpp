265a4f2a0b84c18a6ac24987f3c029730143add6
Suahibawwad123 <newthe707@gmail.com>
2022-12-22 18:13:40 +0300

    getBox implementation

--- main.cpp
+++ main.cpp
@@ -22,17 +22,17 @@ public:
     ~Image();
 
     int *getBox(int pixelX, int pixelY, int boxWidth, int boxHeight);
-    int getMedian(int size);
+    //int getMedian(int size);
     
     int getPixel(int pixelX, int pixelY);
     void setPixel(int pixelX, int pixelY, int value);
 
-    PixelType pixelType(int pixelX, int pixelY);
+    //PixelType pixelType(int pixelX, int pixelY);
 
 };
 
-int loadBalancer(int workerThreads, int rows){
-}
+//int loadBalancer(int workerThreads, int rows){}
+
 int main(int argc, char const *argv[])
 {
     Image inputImg;
@@ -56,3 +56,29 @@ Image::Image(int width, int height)
 Image::~Image()
 {
 }
+void setPixel(int pixelX, int pixelY, int value) {
+
+    image[pixelY][pixelY] = value;
+
+
+};
+int getPixel(int pixelX, int pixelY) {
+
+    return image[pixelY][pixelX];
+
+};
+int* getBox(int pixelX, int pixelY, int boxWidth, int boxHeight) {
+    static int arrBox[9];
+    pixelX--;
+    pixelY--;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++) {
+            if ((pixelX + j) >= boxWidth || (pixelY + i) >= boxHeight || (pixelX + j) < 0 || (pixelY + i) < 0)
+                arrBox[i * 3 + j] = 0;
+            else
+                arrBox[i * 3 + j] = image[pixelY + i][pixelX + j];
+        }
+    }
+    return arrBox;
+};
